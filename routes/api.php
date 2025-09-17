8308872c7b1cf4e1c683bec6702ba653d4c883e5
Yergiroska Aguirre <yergiroska77@gmail.com>
2025-09-17 13:31:46 +0200

    Elimina la autenticación de las rutas
    Elimina el middleware de autenticación de las rutas de productos, compañías y clientes.
    
    También elimina las rutas y el controlador de autenticación.
    
    Esta acción simplifica la configuración de las rutas de la API.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,4 @@
 <?php
-
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CustomerController;
@@ -8,14 +6,13 @@ use Illuminate\Support\Facades\Route;
 
 // Ruta de prueba para verificar que funciona
 Route::get('/test', function () {
-    return response()->json(['message' => 'API funcionando correctamente']);
+    return response()->json(['message' => 'API funcionando correctamente desde Laravel']);
 });
 
 
 // Rutas para el controlador de productos
 Route::prefix('products')
     ->name('products.')
-    ->middleware('auth')
     ->group(function () {
         Route::get('/', [ProductController::class, 'listProducts'])->name('lists');  // Listar
         Route::post('/save', [ProductController::class, 'store'])->name('store'); // Guardar nuevo
@@ -26,7 +23,6 @@ Route::prefix('products')
 
 Route::prefix('companies')
     ->name('companies.')
-    ->middleware('auth')
     ->group(function () {
         Route::get('/', [CompanyController::class, 'listCompanies'])->name('lists');  // Listar
         Route::post('/save', [CompanyController::class, 'store'])->name('store'); // Guardar nuevo
@@ -36,17 +32,10 @@ Route::prefix('companies')
 
 Route::prefix('customers')
     ->name('customers.')
-    ->middleware('auth')
     ->group(function () {
         Route::get('/', [CustomerController::class, 'listCustomers'])->name('lists');  // Listar
         Route::post('/save', [CustomerController::class, 'store'])->name('store'); // Guardar nuevo
         Route::put('/{id}/update', [CustomerController::class, 'update'])->name('update');     // Actualizar
         Route::delete('/{id}/delete', [CustomerController::class, 'destroy'])->name('destroy'); // Eliminar
 });
-
-// Rutas de autenticación
-
-    Route::post('/auth', [AuthController::class, 'login'])->name('post.login');
-    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-    //Route::get('me', [AuthController::class, 'me'])->middleware('auth');
 
